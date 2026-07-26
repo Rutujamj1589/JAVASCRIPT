68e2a4bb4ff9f4b4836e7480dacfa7509c39d641
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-07-26 22:18:36 +0530

    completed the different Data tyeps in JavaScript

--- variables_and_datatypes.js
+++ variables_and_datatypes.js
@@ -15,3 +15,38 @@ let fullName=Rutuja;
 let Price= 78.99;
 console.log(fullName);
 console.log(price);
+const PI;
+PI=3.14;
+
+//object declaration
+
+const student = {
+    fullName : "Rutuja",
+    age :21,
+    roll_no :16,
+    sub : "math"
+};
+
+console.log(student["age"]);
+console.log(student.age);
+student["age"] = student["age"] + 1;
+
+
+
+// question
+const product={
+    fullName : "ball pen",
+    rating :4,
+    offer : 5,
+    price :270
+};
+
+
+//question
+const profile={
+    username: "rutuja jadhav",
+    isFollow: false,
+    followers: 123,
+    following: 2344
+};
+
