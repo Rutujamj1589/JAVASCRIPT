72ee0c5089a96436765936f11778662ba0330b3b
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-07-26 18:57:19 +0530

    Explored  the different data tyeps

--- variables_and_datatypes.js
+++ variables_and_datatypes.js
@@ -1,1 +1,17 @@
 console.log("Welcome to the javascript tutorial!")
+name = "Rutuja";
+console.log(name);
+age=89;
+price=78.89;
+console.log(age);
+console.log(price);
+x=null;
+y=undefined;
+isfollow=false;
+notfollow=true;
+// let const var
+
+let fullName=Rutuja;
+let Price= 78.99;
+console.log(fullName);
+console.log(price);
