53c9bea9f6d6f277f5b7f292f4b89d3367aea7d5
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-07-27 23:44:38 +0530

    completed the operators and conditional statements.

---
+++ operators_and_conditional_statements.js
@@ -0,0 +1,99 @@
+// comments
+// this is the single line comment.
+/*this is 
+multiline 
+comment. */
+
+
+// arithmetic operators:
+
+// let a=2;
+// let b=3;
+// let c1=a+b;
+// let c2=a-b;
+// let c3=a*b;
+// let c4=a/b;
+// let c5=a%b;
+// console.log("a+b=",c1,c2,c3,c4,c5);
+
+
+// unary operator:
+// let a=2;
+// let b=3;
+
+// console.log("a = " , a , " & b = ",b);
+
+// a=a+1;
+// b=b+1;
+// a++;
+// b++;
+// console.log("a= ",a , "b = ",b );
+
+
+//  Assignment operator:
+
+// let a=9;
+// let b=5;
+// a += 5; // a+=5 is also writen as a = a + 5  
+// similarly we can write all the operators like this way. 
+// console.log("a= ", a );
+
+
+
+
+// comparison operator:
+
+// this operators sre used to check the conditon 
+// and to comapre the value of operators.
+
+/*the different types of operators are as follows:
+1.== 
+2.===
+3.!==
+4.!===
+5.<
+6.<=
+7.>
+8.>=
+*/
+
+
+/*logical operators:
+
+1. Logical AND (&&)
+
+2. Logical OR (||)
+
+3. Logical NOT (!)*/
+
+
+// ternary operators:
+// syntax :
+// condition ? true output : false output
+
+
+
+// conditional Statements:
+//1. if statement:
+ 
+// let age=25;
+// if(age>18){
+//     console.log("You can vote");
+// }
+
+//2.if-else 
+//3.else- if 
+
+// let name = prompt("hello");
+// console.log(name);
+
+
+let num = prompt("enter a number: ")
+
+if (num % 5 === 0){
+    console.log("The number is multiple of five.")
+
+}
+else{
+    console.log("number is not multiple of five.")
+}
