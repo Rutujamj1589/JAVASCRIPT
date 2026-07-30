f2055c7d2929b62ab73dd97996cfcf8cfc742c34
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-07-30 23:02:44 +0530

    explored the diiferent types of loops in JavaScript also learn about the Template literal.

---
+++ loops_and_string.js
@@ -0,0 +1,111 @@
+// for loop:
+
+// for(let i=0; i<=4;i++){
+//     console.log("Rutuja");
+// }
+
+
+// let sum=0;
+// let n =9;
+// for(let i = 1; i<=n;i++){
+//     sum = sum + i; 
+// }
+// console.log("the sum is= ",sum);
+
+
+// while loop:
+
+// let i=1;
+// let sum =0;
+// while(i<=5){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log("sum is = ",sum);
+
+
+// do-while loop: 
+
+// let i=0;
+// do{
+//     console.log("rutuja");
+//     i++;
+
+// }while(i<=4);
+
+
+// for-of loop:
+
+// let str = "javascript";
+// let size = 0;
+// for(let i of str){
+//     console.log("i is = ",i);
+//     size++;
+// }
+// console.log("Size is = ",size);
+
+
+// for-in loop:
+// let student ={
+//     name : "rutuja jadhav ",
+//     age : 23,
+//     cgpa :9.7,
+//     ispass : true
+// };
+
+// for(let key in student){
+//     // console.log("key=", key,"Value=", student[key]);
+//     console.log("key =", key , " & values = ", student[key]);
+    
+// }
+
+
+// practice questions :
+
+//1.
+
+// for(let i = 0 ; i<=100;i++){
+//     // console.log(i);
+//     if(i%2==0){
+//         console.log(i);
+//     }
+    
+// }
+
+// 2.
+
+// let guessnum = 15;
+// let usernum = prompt("enter you guess number : ");
+
+// while(usernum != guessnum){
+//     usernum = prompt("you guess wrong number. Guess agin! ");
+// };
+
+// console.log("YaaAAA! you guess the correct number.");
+
+
+
+
+//  strings:
+
+// let str = "rutuja jadhav";
+// let st = 'rutuja';
+// console.log(st.length)
+// console.log(str.length)
+// console.log(str[1])
+
+
+//  templete literals:
+
+// let str = `this is a template literal`;
+// console.log(str);
+
+// let str = "rutuja";
+// let newstr = str.toUpperCase()
+// console.log(str);
+// console.log(newstr)
+
+//  practice question:
+
+
+prompt("enter your name without spaces :")
