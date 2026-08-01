aa850f474e37f7eaa2c88537fe3c35c00d15e239
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-08-01 10:36:28 +0530

    Completed Arrays in Javascript eith different topics such as Map, Reduce, Filter etc.,

--- function_And_Methods.js
+++ function_And_Methods.js
@@ -30,3 +30,77 @@ function sum(a,b){
 const arrowfuc = (a , b) => {
     console.log(a + b );
 };
+
+
+// practice question:
+
+function countVowels(str){
+    let count=0;
+    for(const char of str){
+        if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
+            count++;
+        }
+    }
+    console.log(count);
+};
+
+// countVowels("rutuja jadhav");
+
+
+// forEach loop:
+
+let arr = [1,2,3,4,5];
+arr.forEach(function printvalue(val){
+    console.log(val);
+
+})
+
+
+// practice question:
+let num=[2,4,3,5,6];
+num.forEach((num) =>{
+    console.log(num*num);
+})
+
+
+
+
+// filter:
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+let newArr = arr.filter((val) => {
+    return val%2 == 0;
+})
+console.log(newArr);
+
+
+// practice question:
+
+let arr = [34,78,90,99,89,99,98,100];
+
+let newArr1 = arr.filter((val) =>{
+    return val > 90; 
+})
+console.log(newArr1);
+
+//practice question:
+
+let n = prompt("enter a number:");
+let arr = [];
+
+for(i=1; i<=n; i++){
+    arr[i - 1]=i;
+}
+console.log(arr);
+
+let sum = arr.reduce((res , curr) => {
+    return res + curr ;
+});
+
+console.log("sum = ", sum);
+
+let fact = arr.reduce((res , curr) => {
+    return res * curr ;
+});
+
+console.log("factorial = ",fact);
