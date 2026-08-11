83d6d8957b88dd72b7364dc402759a4a77826928
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-08-11 15:41:36 +0530

    Completed the chapter classes and objects in js.

--- class_and_obj.js
+++ class_and_obj.js
@@ -1,66 +1,66 @@
 //object creation
-// const student = {
-//     fullName : "rutuja mahadev jadhav",
-//     marks : 94.4,
-//     printMarks : () => {
-//         console.log("Hello");
-//     }
-// }; 
+const student = {
+    fullName : "rutuja mahadev jadhav",
+    marks : 94.4,
+    printMarks : () => {
+        console.log("Hello");
+    }
+}; 
 
-// const employee = {
-//     calcTax1 () {
-//         console.log("Tax rate is 10% ");
-//     }
-    // calcTax2 : function() {
-    //     console.log("Tax rate is 20%")
-    // }
-//};
+const employee = {
+    calcTax1 () {
+        console.log("Tax rate is 10% ");
+    },
+    calcTax2 : function() {
+        console.log("Tax rate is 20%")
+    }
+};
 
-// const karan = {
-//     salary : 50000
-// };
-// karan.__proto__ = employee; // prototype in js.
-
-
-// classes in javascript :
-
-// class bmwcar{
-//     constructor(){
-//         console.log("Creating an object");
-//     }
-//     start(){
-//         console.log("start");
-//     }
-
-//     stop(){
-//         console.log("stop");
-//     }
-
-//     setbrand(brand){
-//         this.brandName = brand;
-//     }
-
-// }
-
-// let obj = new bmwcar();
-// obj.setbrand("fortuner");
-
-// constructor
-
-// class student1 {
-//     constructor(name , age ,marks ){
-//         this.name = name,
-//         this.age = age,
-//         this.marks = marks
-//     }
-// } ;
-
-// let s1 = new student1("rutuja",22,98);
-// console.log(s1.name);
+const karan = {
+    salary : 50000
+};
+karan.__proto__ = employee; // prototype in js.
 
 
+//classes in javascript :
 
-// inheritance 
+class bmwcar{
+    constructor(){
+        console.log("Creating an object");
+    }
+    start(){
+        console.log("start");
+    }
+
+    stop(){
+        console.log("stop");
+    }
+
+    setbrand(brand){
+        this.brandName = brand;
+    }
+
+}
+
+let obj = new bmwcar();
+obj.setbrand("fortuner");
+
+//constructor
+
+class student1 {
+    constructor(name , age ,marks ){
+        this.name = name,
+        this.age = age,
+        this.marks = marks
+    }
+} ;
+
+let s1 = new student1("rutuja",22,98);
+console.log(s1.name);
+
+
+
+//inheritance 
 
 class parent {
     hello(){
@@ -100,3 +100,22 @@ class doctor extends person{
 }
 
 let obj1 = new doctor();
+
+
+
+//practice problem:
+
+let data = "secret information";
+class user {
+    constructor(name,email){
+        this.name =name,
+        this.email = email
+    }
+
+    viewData() {
+        console.log("data = ",data);
+    }
+}
+
+let stu1 = new user("Rutuja","rutu@gmail.com");
+let stu2 = new user("Shrijaa","shri@gmail.com");
