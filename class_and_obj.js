b3de0d37149ab7bcbf17582b0f22f4dd6265ea06
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-08-11 12:03:32 +0530

    Completed Inheritance concept in js

--- class_and_obj.js
+++ class_and_obj.js
@@ -1,59 +1,102 @@
 //object creation
-const student = {
-    fullName : "rutuja mahadev jadhav",
-    marks : 94.4,
-    printMarks : () => {
-        console.log("Hello");
-    }
-}; 
+// const student = {
+//     fullName : "rutuja mahadev jadhav",
+//     marks : 94.4,
+//     printMarks : () => {
+//         console.log("Hello");
+//     }
+// }; 
 
-const employee = {
-    calcTax1 () {
-        console.log("Tax rate is 10% ");
-    }
+// const employee = {
+//     calcTax1 () {
+//         console.log("Tax rate is 10% ");
+//     }
     // calcTax2 : function() {
     //     console.log("Tax rate is 20%")
     // }
-};
+//};
 
-const karan = {
-    salary : 50000
-};
-karan.__proto__ = employee; // prototype in js.
+// const karan = {
+//     salary : 50000
+// };
+// karan.__proto__ = employee; // prototype in js.
 
 
 // classes in javascript :
 
-class bmwcar{
-    constructor(){
-        console.log("Creating an object");
-    }
-    start(){
-        console.log("start");
-    }
+// class bmwcar{
+//     constructor(){
+//         console.log("Creating an object");
+//     }
+//     start(){
+//         console.log("start");
+//     }
 
-    stop(){
-        console.log("stop");
-    }
+//     stop(){
+//         console.log("stop");
+//     }
 
-    setbrand(brand){
-        this.brandName = brand;
-    }
+//     setbrand(brand){
+//         this.brandName = brand;
+//     }
 
-}
+// }
 
-let obj = new bmwcar();
-obj.setbrand("fortuner");
+// let obj = new bmwcar();
+// obj.setbrand("fortuner");
 
 // constructor
 
-class student1 {
-    constructor(name , age ,marks ){
-        this.name = name,
-        this.age = age,
-        this.marks = marks
-    }
-} ;
+// class student1 {
+//     constructor(name , age ,marks ){
+//         this.name = name,
+//         this.age = age,
+//         this.marks = marks
+//     }
+// } ;
 
-let s1 = new student1("rutuja",22,98);
-console.log(s1.name);
+// let s1 = new student1("rutuja",22,98);
+// console.log(s1.name);
+
+
+
+// inheritance 
+
+class parent {
+    hello(){
+        console.log("helllo");
+    }
+}
+
+class child extends parent{}
+
+let obj = new child();
+
+
+
+class person {
+    eat(){
+        console.log("Eat");
+
+    }
+
+    sleep(){
+        console.log("sleep");
+    }
+}
+
+class Engineer extends person{
+    work(){
+        console.log("solve problem, build something");
+    }
+}
+
+let rutuja = new Engineer();
+
+class doctor extends person{
+    work(){
+        console.log("See pateints");
+    }
+}
+
+let obj1 = new doctor();
