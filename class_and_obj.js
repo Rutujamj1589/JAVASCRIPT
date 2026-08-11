a6ed3998e0061baa77b26d6827bfe4ce8bf68493
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-08-11 11:08:25 +0530

    learned Constructors in JS

--- class_and_obj.js
+++ class_and_obj.js
@@ -20,3 +20,40 @@ const karan = {
     salary : 50000
 };
 karan.__proto__ = employee; // prototype in js.
+
+
+// classes in javascript :
+
+class bmwcar{
+    constructor(){
+        console.log("Creating an object");
+    }
+    start(){
+        console.log("start");
+    }
+
+    stop(){
+        console.log("stop");
+    }
+
+    setbrand(brand){
+        this.brandName = brand;
+    }
+
+}
+
+let obj = new bmwcar();
+obj.setbrand("fortuner");
+
+// constructor
+
+class student1 {
+    constructor(name , age ,marks ){
+        this.name = name,
+        this.age = age,
+        this.marks = marks
+    }
+} ;
+
+let s1 = new student1("rutuja",22,98);
+console.log(s1.name);
