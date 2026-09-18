8cf68319cdf8a8ff9b67fbcd46141743eca76aae
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-09-18 23:58:40 +0530

    Completed the currency convertor in js.

--- currency_convertor.js
+++ currency_convertor.js
@@ -1,6 +1,10 @@
+const BASE_URL = "https://cdn,jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
 const dropdown = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
-
+const fromcurr = document.querySelector(".from selector");
+const tocurr = document.querySelector(".to selector");
+const msg = document.querySelector(".final-msg");
 for(let select of dropdown){
     for(currcode in countryList){
         let newOption = document.createElement("option");
@@ -27,6 +31,22 @@ const updateFlag = (element) =>{
     let newsrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
     let img = element.parentElement.querySelector("img" );
     img.src = newsrc;
-}
+};
 
-btn.addEventListener()
+
+btn.addEventListener("click", async(evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1);{
+    amtVal = 1 ;
+    amount.value = "1";
+  }
+
+  const URL = `${BASE_URL}/${fromcurr.value.toLowerCase()}/${tocurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  data = await response.json();
+  let rate = data[tocurr.value.toLowerCase()];
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromcurr.value} = ${finalAmount}${tocurr}`;
+});
