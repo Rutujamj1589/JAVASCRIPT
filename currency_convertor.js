c40d1005cfa7f5a29496daa62dcd84b4ddb3e1be
Rutujamj1589 <rutujamjadhav5002@gmail.com>
2026-09-18 23:28:59 +0530

    Modify the dropdown section.

--- currency_convertor.js
+++ currency_convertor.js
@@ -1,5 +1,5 @@
-const dropdown = document.querySelectorAll(".dropdown select")
-
+const dropdown = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
 
 for(let select of dropdown){
     for(currcode in countryList){
@@ -25,5 +25,8 @@ const updateFlag = (element) =>{
     let currcode = element.value;
     let countryCode = countryList[currcode];
     let newsrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
-    
+    let img = element.parentElement.querySelector("img" );
+    img.src = newsrc;
 }
+
+btn.addEventListener()
